cc5c8075b083b9ac6fb717eb1e124ef4286c6127
dmytropolyt <drago5490@gmail.com>
2023-04-20 21:15:01 +0300

    allowed hosts

--- app/app/settings.py
+++ app/app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', '0'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS.extend(
     filter(None, os.environ.get('ALLOWED_HOSTS').split(','))
 )
